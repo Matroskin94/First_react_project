84ef935dc3a2b644dd1e20b8c9e2524a30bf663e
Matroskin94 <anton1994anton@mail.ru>
2018-01-30 14:00:59 +0300

    Initial commit for first react project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {render} from 'react-dom'
+import Article from './Article'
+
+function App(){
+	return (
+		<div>
+			<h1> Application name</h1>
+			<Article />
+		</div>
+		);
+}
+
+export default App
---
+++ src/components/Article.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {render} from 'react-dom'
+import App from './App'
+
+function Article(){
+ 	return (
+     	<div className="Article">
+        	<h1> Hello React</h1>
+        	<p>Article Text	</p>
+        </div>
+    )
+}
+
+export default Article
